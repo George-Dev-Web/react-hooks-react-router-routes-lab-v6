0eb5ec412a05b9c3367ab751236a1bb448647bb2
George <georgemwazuna3@gmail.com>
2025-04-26 00:10:25 +0300

    fails

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,10 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <NavLink className="navbar" to="/">Home</NavLink>
+      <NavLink className="navbar" to="/actors">Actors</NavLink>
+      <NavLink className="navbar" to="/directors">Directors</NavLink>
+      <NavLink className="navbar" to="/movie">Movie</NavLink>
     </nav>
     );
 };
